a54d4a088f0204ebc08900db58162386b80817b8
Markus Krogemann <markus@krogemann.de>
2016-01-12 18:26:18 +0100

    release 0.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BirdWatch.Mixfile do
   def project do
     [
       app: :bird_watch,
-      version: "0.0.1",
+      version: "0.1.0",
       elixir: "~> 1.0",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix] ++ Mix.compilers,
